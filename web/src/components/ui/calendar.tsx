1d5df8e8ae69f738e181f87b6564487ce57d5112
moto-taka <pspdswii3@gmail.com>
2025-06-11 12:33:25 +0900

    fix: react-day-picker v9のChevronコンポーネント実装を修正
    - IconLeft/IconRightからChevronコンポーネントに変更
    - orientationプロパティで左右のアイコンを切り替える実装に修正
    - ビルドとtype-checkが正常に通ることを確認

--- web/src/components/ui/calendar.tsx
+++ web/src/components/ui/calendar.tsx
@@ -52,12 +52,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        Chevron: ({ orientation }) =>
-          orientation === 'left' ? (
-            <ChevronLeft className="h-4 w-4" />
-          ) : (
-            <ChevronRight className="h-4 w-4" />
-          ),
+        Chevron: ({ orientation }) => {
+          if (orientation === 'left') {
+            return <ChevronLeft className="h-4 w-4" />;
+          }
+          return <ChevronRight className="h-4 w-4" />;
+        },
       }}
       {...props}
     />
